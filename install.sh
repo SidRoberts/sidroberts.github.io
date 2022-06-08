c0cb43b980eb4b208b4cb763d6c4ee379b788612
Sid Roberts <sid@sidroberts.co.uk>
2022-06-08 12:53:32 +0900

    Added Arch install script.

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+echo "============================="
+echo "= Sid's Arch Install Script ="
+echo "============================="
+echo
+echo "This will DESTROY any data on /dev/sda."
+echo
+echo "If you are not me, then you probably shouldn't be running this! ;)"
+echo
+
+read -p "To continue, type 'yes': " confirm
+
+if [[ "$confirm" != "yes" ]]; then
+    echo "Install aborted."
+    exit 1
+fi
+
+pacman -Sy git
+
+git clone https://github.com/SidRoberts/arch-install-script.git
+
+cd arch-install-script/
+
+./start.sh
