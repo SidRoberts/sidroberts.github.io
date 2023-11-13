4be4d03398b19d487395e588a83b4574d4fc3de9
Sid Roberts <sid@sidroberts.co.uk>
2023-11-13 20:11:36 +0900

    Transitioned Linux setup script to Fedora.

--- install.sh
+++ install.sh
@@ -2,26 +2,8 @@
 
 set -e
 
-echo "============================="
-echo "= Sid's Arch Install Script ="
-echo "============================="
-echo
-echo "This will DESTROY any data on /dev/sda."
-echo
-echo "If you are not me, then you probably shouldn't be running this! ;)"
-echo
+wget https://github.com/SidRoberts/fedora-install-script/archive/refs/heads/main.zip
 
-read -p "To continue, type 'yes': " confirm
+unzip main.zip
 
-if [[ "$confirm" != "yes" ]]; then
-    echo "Install aborted."
-    exit 1
-fi
-
-pacman -Sy git --noconfirm
-
-git clone https://github.com/SidRoberts/arch-install-script.git
-
-cd arch-install-script/
-
-./start.sh
+./fedora-install-script-main/setup.sh
